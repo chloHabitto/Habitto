a4ee88e7bdb6e6d16edb434c20fe6e94b3304243
snoopchlo <chloelee00516@gmail.com>
2025-07-27 22:35:49 +0200

    Refactor hex color generation in Color extension for improved clarity; change variable declaration to a constant for better code readability.

--- Habit.swift
+++ Habit.swift
@@ -111,7 +111,7 @@ extension Color: Codable {
         let r = Float(components[0])
         let g = Float(components[1])
         let b = Float(components[2])
-        var hex = String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        let hex = String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
         return hex
     }
 } 
